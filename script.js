25449bdf4c79701ed7deacc25906f386d6d45bac
Dugsteer <dugsteer@gmail.com>
2021-11-16 12:03:11 +0100

    First commit

--- script.js
+++ script.js
@@ -6,15 +6,15 @@ const ball = document.getElementById('ball');
 const blue = window.matchMedia("(max-width: 800px)");
 const cheers = new Audio('sounds/cheers.mp3');
 
-function deactivate(){
-    ball.classList.remove('action');
-}
+// function deactivate(){
+//     ball.classList.remove('action');
+// }
 
-function kickball(){
-    ball.classList.add('action');
-    cheers.play();
-    setTimeout(deactivate, 2000);
-}
+// function kickball(){
+//     ball.classList.add('action');
+//     cheers.play();
+//     setTimeout(deactivate, 2000);
+// }
 
 
 
@@ -22,30 +22,30 @@ function kickball(){
 
 //Play applause 
 
-function clapping(){
-    const claps = new Audio("sounds/applause.wav");
-    claps.play();
-}
+// function clapping(){
+//     const claps = new Audio("sounds/applause.wav");
+//     claps.play();
+// }
 
 // Show and hide about info
 function show(){info.classList.toggle('xxc');};
 function unshow(){info.classList.remove('xxc');};
 
-//Center the loader wherever the page is
-function checkForTop (){loader.style.top = (window.pageYOffset) + "px";}
-checkForTop();
+// Center the loader wherever the page is
+// function checkForTop (){loader.style.top = (window.pageYOffset) + "px";}
+// checkForTop();
 
 //Manage the loader
-function stopLoader() {loader.classList.add("stop");}
-function startLoader(){loader.classList.remove("stop"); setTimeout(stopLoader, 1000);}
-setTimeout(stopLoader, 1000);
+// function stopLoader() {loader.classList.add("stop");}
+// function startLoader(){loader.classList.remove("stop"); setTimeout(stopLoader, 1000);}
+// setTimeout(stopLoader, 1000);
 
 //Stop the sidebar appearing on small screens with matchmedia
 function myFunction(x) {if (blue.matches) {sidey.classList.remove("block");}}
 function toggler() {sidey.classList.toggle("block");}
 myFunction(blue);
 
-//Change main pic
+// Change main pic
 
 // setInterval(picChanger, 5000);
 // picChanger();
@@ -56,11 +56,11 @@ myFunction(blue);
 
 //FLIP CARDS
 
-const inner = document.getElementById('inner');
+// const inner = document.getElementById('inner');
 
-function flipMe(){
-    inner.classList.toggle('turnMe');
-}
+// function flipMe(){
+//     inner.classList.toggle('turnMe');
+// }
 
 //CHANGE CARDS
 const count = document.getElementById('count');
@@ -68,40 +68,40 @@ let imageArt = document.getElementById('image-art');
 let imageText = document.getElementById('image-text');
 
 var imageList = [
-    "archery.jpg",
-    "badminton.jpg",
-    "basketball.jpg",
-    "boxing.jpg",
-    "climbing.jpg",
-    "darts.jpg",
-    "diving.jpg",
-    "driving.jpg",
-    "football.jpg",
-    "golf.jpg",
-    "gymnastics.jpg",
-    "hang-gliding.jpg",
-    "hiking.jpg",
-    "hockey.jpg",
-    "horse-riding.jpg",
-    "ice-skating.jpg",
-    "karate.jpg",
-    "paddle-surfing.jpg",
-    "rugby.jpg",
-    "running.jpg",
-    "sailing.jpg",
-    "skiing.jpg",
-    "surfing.jpg",
-    "swimming.jpg",
-    "table-tennis.jpg",
-    "tennis.jpg",
-    "weight-lifting.jpg",
-    "windsurfing.jpg",
-    "yoga.jpg"
+  "get-up.jpeg",
+  "brush-your-teeth.jpeg",
+  "take-a-shower.jpeg",
+  "get-dressed.jpeg",
+  "put-on-makeup.jpeg",
+  "comb-your-hair.jpeg",
+  "have-breakfast.jpeg",
+  "feed-the-dog.jpeg",
+  "walk-the-dog.jpeg",
+  "do-exercise.jpeg",
+  "catch-the-bus.jpeg",
+  "go-to-school.jpeg",
+  "drive-a-car.jpeg",
+  "go-to-work.jpeg",
+  "drink-water.jpeg",
+  "ride-a-bike.jpeg",
+  "clean-the-house.jpeg",
+  "use-the-telephone.jpeg",
+  "have-lunch.jpeg",
+  "draw-pictures.jpeg",
+  "listen-to-music.jpeg",
+  "watch-tv.jpeg",
+  "go-shopping.jpeg",
+  "play-videogames.jpeg",
+  "read-a-book.jpeg",
+  "do-your-homework.jpeg",
+  "cook-the-dinner.jpeg",
+  "have-a-bath.jpeg",
+  "go-to-sleep.jpeg",
 ];
 
 
 let y = imageText.innerHTML;
-let z = y.concat(".jpg");
+let z = y.concat(".jpeg");
 let x = imageList.indexOf(z);
 
 
@@ -109,12 +109,12 @@ function goForwards() {
   if (x < imageList.length - 1) {
     x++;
     imageArt.src =  `/img/${imageList[x]}`;
-   imageText.textContent = `${imageList[x].slice(0, -4).replace("-", " ")}`;
+   imageText.textContent = `${imageList[x].slice(0, -5).replaceAll("-", " ")}`;
     count.innerHTML = `${x + 1}/${imageList.length}`;
     return x;
   } else {
     imageArt.src = ` /img/${imageList[imageList.length - 1]}`;
-    imageText.textContent = `${imageList[imageList.length - 1].slice(0, -4).replace("-", " ")}`;
+    imageText.textContent = `${imageList[imageList.length - 1].slice(0, -5).replaceAll("-", " ")}`;
     count.innerHTML = `${imageList.length}/${imageList.length}`;
     x = 0;
     x = imageList.length -1;
@@ -126,12 +126,12 @@ function goBackwards() {
   if (x > 0) {
     x--;
     imageArt.src =  `/img/${imageList[x]}`;
-    imageText.textContent = `${imageList[x].slice(0, -4).replace("-", " ")}`;
+    imageText.textContent = `${imageList[x].slice(0, -5).replaceAll("-", " ")}`;
     count.innerHTML = `${x + 1}/${imageList.length}`;
     return x;
   } else {
     imageArt.src = ` /img/${imageList[0]}`;
-    imageText.textContent = `${imageList[0].slice(0, -4).replace("-", " ")}`;
+    imageText.textContent = `${imageList[0].slice(0, -5).replaceAll("-", " ")}`;
     count.innerHTML = `${1}/${imageList.length}`;
     x = 0;
     return x;
